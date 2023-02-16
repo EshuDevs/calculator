e1f960e9aefc7d092ee96583eeb9e5759c8988f3
Eshuyaleo <115549948+Eshuyaleo@users.noreply.github.com>
2023-02-16 16:33:54 +0100

    updated the script.js file

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-// Get the display element
 const display = document.querySelector(".screen-number");
 
-// Get all the number buttons
 const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator")
 
-// Add event listeners to each number button
 numberButtons.forEach(button => {
   button.addEventListener("click", () => {
-    // Get the number that was clicked
     const number = button.innerHTML;
-    
-    // Update the display with the number
     display.innerHTML += number;
   });
 });
 
-
-// Create a function to delete the last character from the display
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const operator = button.innerHTML;
+        display.innerHTML += operator
+    })
+})
 function deleteLastCharacter() {
   display.innerHTML = display.innerHTML.slice(0, -1);
 }
 
-// Get the delete button
 const deleteButton = document.querySelector(".delete");
 
-// Add an event listener to the delete button
 deleteButton.addEventListener("click", deleteLastCharacter);
 
 function clearNumbers(){
@@ -33,3 +30,6 @@ function clearNumbers(){
 const clearButton = document.querySelector(".clear");
 
 clearButton.addEventListener("click", clearNumbers);
+
+
+
