6c49799e4a2866bc7a645d0768d52090024042cc
Eshuyaleo <115549948+Eshuyaleo@users.noreply.github.com>
2023-02-17 14:44:13 +0100

    updated the script.js file

--- script.js
+++ script.js
@@ -1,35 +1,91 @@
-const display = document.querySelector(".screen-number");
+let display = document.querySelector(".screen-number");
 
-const numberButtons = document.querySelectorAll(".number");
-const operatorButtons = document.querySelectorAll(".operator")
+let numberButtons = document.querySelectorAll(".number");
+let operatorButtons = document.querySelectorAll(".operator")
+let equalsButton = document.querySelector(".result")
 
-numberButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    const number = button.innerHTML;
-    display.innerHTML += number;
-  });
-});
+let prevNumber = null;
+let currentNumber = null;
+
 
 operatorButtons.forEach(button => {
     button.addEventListener("click", () => {
-        const operator = button.innerHTML;
+        let operator = button.innerHTML;
         display.innerHTML += operator
+
+        prevNumber = currentNumber;
+
+        currentNumber = null;
+
+        operatorButtons.forEach(button => button.classList.remove("operator-selected"));
+        button.classList.add("operator-selected");
     })
 })
 function deleteLastCharacter() {
   display.innerHTML = display.innerHTML.slice(0, -1);
+
+  currentNumber = display.innerHTML;
 }
 
-const deleteButton = document.querySelector(".delete");
+let deleteButton = document.querySelector(".delete");
 
 deleteButton.addEventListener("click", deleteLastCharacter);
 
 function clearNumbers(){
     display.innerHTML = ""
+    
+    currentNumber = display.innerHTML
 }
-const clearButton = document.querySelector(".clear");
+let clearButton = document.querySelector(".clear");
 
 clearButton.addEventListener("click", clearNumbers);
 
+function calculate(operator){
+  if(prevNumber  !== null && currentNumber !== null){
+    let result;
+
+    switch (operator){
+      case "+":
+        result = parseFloat(prevNumber) + parseFloat(currentNumber);
+        break;
+      case "-":
+        result = parseFloat(prevNumber) - parseFloat(currentNumber);
+        break;
+      case "×":
+        result = parseFloat(prevNumber) * parseFloat(currentNumber);
+        break;
+      case "÷":
+        result = parseFloat(prevNumber) / parseFloat(currentNumber);
+        break;
+      default:
+        result = "Error"
+    }
+    prevNumber  = result.toString();
+    currentNumber = null;
+  }
+}
+
+equalsButton.addEventListener("click", () => {
+
+  let operator = document.querySelector(".operator-selected").innerHTML;
+
+  calculate(operator);
+
+  display.innerHTML = prevNumber 
+})
+
+numberButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    let number = button.innerHTML;
+   
+    if(currentNumber === null){
+      currentNumber = number
+    }
+    else{
+      currentNumber += number
+    }
+    display.innerHTML = currentNumber
+  });
+});
 
 
